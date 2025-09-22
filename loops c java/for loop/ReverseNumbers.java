687508b3e711c4e1b60bedd9177229d261c48d6b
vishaldeshmukh34 <vishaldeshmukh7972@gmail.com>
2025-09-22 22:41:40 +0530

    Add ReverseNumbers program to print 10 to 1

---
+++ loops c java/for loop/ReverseNumbers.java
@@ -0,0 +1,11 @@
+public class ReverseNumbers {
+    
+    public static void main(String[] args) {
+        //Question: Print numbers in reverse order from 10 to 1 using a for loop.
+
+        for(int num = 10 ; num >= 1 ; num--)
+        {
+            System.out.println(num);
+        }
+    }
+}
