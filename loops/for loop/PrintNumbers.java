ddadde094d90249fce751cecafa3ec90e021124c
vishaldeshmukh34 <vishaldeshmukh7972@gmail.com>
2025-09-22 22:36:29 +0530

    Add PrintNumbers program to print 1 to 10

---
+++ loops/for loop/PrintNumbers.java
@@ -0,0 +1,12 @@
+public class PrintNumbers {
+    public static void main(String[] args) {
+        
+        //Question: Write a program to print numbers 1, 2, 3 … up to 10.
+
+        for(int num = 1; num <= 10 ; num++)
+        {
+            System.out.println(num);
+        }
+    }
+
+}
